f0ad7c176e671881923ede6da7bc7eaee94e9e3d
Alexander Melde <2115644+AlexanderMelde@users.noreply.github.com>
2025-07-16 23:09:35 +0200

    Publish application to Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/platane/', // Set base path for serving from subdirectory
+  base: '/', // Set base path for serving from subdirectory
 })
