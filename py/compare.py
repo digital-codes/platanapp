36fd901c49db847492144fb6e2c2c22a67efba15
Andreas Kugel <andreas.kugel@web.de>
2025-07-20 09:15:31 +0200

    prepare climate prompt

--- py/compare.py
+++ py/compare.py
@@ -13,6 +13,14 @@ else:
     from datetime import timezone
     current_time = datetime.now(timezone.utc)
 
+import locale
+# Set locale to German for date formatting
+try:
+    locale.setlocale(locale.LC_TIME, 'de_DE.UTF-8')
+except locale.Error:
+    # Fallback for systems where 'de_DE.UTF-8' is not available
+    locale.setlocale(locale.LC_TIME, 'deu')
+
 current_month = current_time.month
 print(f"Current month: {current_month}")
 
@@ -40,40 +48,70 @@ print(f"Current temperature: {currVal}")
 
 checks = {}
 
-refVal = window_7d['temperature'].iloc[0]
-checks['tmean_7d'] = currVal - refVal
-refVal = window_7d['mintemp'].iloc[0]
-checks['tmin_7d'] = currVal - refVal
+# Format current date in German style, e.g. "Dienstag, 12 März 2013"
+prompt = f"Heute ist {current_time.strftime("%A, %d %B %Y")}."
+print(prompt)
+
+# 7 days
+# high temp ref
+refVal = abs(window_7d['maxtemp'].iloc[0] - window_7d['temperature'].iloc[0])/2 + window_7d['temperature'].iloc[0]
+#refVal = window_7d['temperature'].iloc[0]
+checks['thigh_7d'] = currVal > refVal
+
+# max temp ref
 refVal = window_7d['maxtemp'].iloc[0]
-checks['tmax_7d'] = currVal - refVal
+checks['tmmax_7d'] = currVal > refVal
 
-refVal = window_30d['temperature'].iloc[0]
-checks['tmean_30d'] = currVal - refVal
-refVal = window_30d['mintemp'].iloc[0]
-checks['tmin_30d'] = currVal - refVal
+# low temp ref
+refVal =  + window_7d['temperature'].iloc[0] - abs(window_7d['temperature'].iloc[0] - window_7d['mintemp'].iloc[0])/2
+# refVal = window_7d['mintemp'].iloc[0]
+checks['tlow_7d'] = currVal < refVal
+
+# low temp ref
+refVal = window_7d['mintemp'].iloc[0]
+checks['tmin_7d'] = currVal < refVal
+
+# 30 day checks
+# high temp ref
+refVal = abs(window_30d['maxtemp'].iloc[0] - window_30d['temperature'].iloc[0])/2 + window_30d['temperature'].iloc[0]
+#refVal = window_7d['temperature'].iloc[0]
+checks['thigh_30d'] = currVal > refVal
+
+# max temp ref
 refVal = window_30d['maxtemp'].iloc[0]
-checks['tmax_30d'] = currVal - refVal
+checks['tmmax_30d'] = currVal > refVal
 
-refVal = longTerm['temperature'].iloc[0]
-checks['tmean_longTerm'] = currVal - refVal
+# low temp ref
+refVal =  + window_30d['temperature'].iloc[0] - abs(window_30d['temperature'].iloc[0] - window_30d['mintemp'].iloc[0])/2
+# refVal = window_7d['mintemp'].iloc[0]
+checks['tlow_30d'] = currVal < refVal
+
+# low temp ref
+refVal = window_30d['mintemp'].iloc[0]
+checks['tmin_30d'] = currVal < refVal
+
+# long term checks
 refVal = longTerm['mintemp'].iloc[0]
-checks['tmin_longTerm'] = currVal - refVal
+checks['tmin_longTerm'] = currVal < refVal
 refVal = longTerm['maxtemp'].iloc[0]
-checks['tmax_longTerm'] = currVal - refVal
+checks['tmax_longTerm'] = currVal > refVal
 
 
 # rainfall
 currVal = current['rainfall'].iloc[0]
 refVal = window_7d['rainfall'].iloc[0]
-checks['rmean_7d'] = currVal - refVal
+checks['rlow_7d'] = currVal < refVal * .5
+checks['rhigh_7d'] = currVal > refVal * 2
 
 refVal = window_30d['rainfall'].iloc[0]
-checks['rmean_30d'] = currVal - refVal
+checks['rlow_30d'] = currVal < refVal * .5
+checks['rhigh_30d'] = currVal > refVal * 2
 
 refVal = longTerm['rainfall'].iloc[0]
-checks['rmean_longTerm'] = currVal - refVal
+checks['rlow_longTerm'] = currVal < refVal * .5
+checks['rhigh_longTerm'] = currVal > refVal * 2
 refVal = longTerm['maxrain'].iloc[0]
-checks['rmax_longTerm'] = currVal - refVal
+checks['rmax_longTerm'] = currVal > refVal
 
 # also compare 7 days to longterm
 # temperature 
